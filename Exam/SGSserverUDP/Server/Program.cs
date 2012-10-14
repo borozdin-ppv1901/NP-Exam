7cb6aa320254ad0270d2f65377f3baaa6d51fb7e
Borozdin Pavel <xstyler@mail.ru>
2012-10-14 23:21:15 +0300

    Show Dot On the screen by cordinats

--- Exam/SGSserverUDP/Server/Program.cs
+++ Exam/SGSserverUDP/Server/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Server
@@ -8,14 +7,14 @@ namespace Server
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SGSserverForm());
         }
     }
 }
